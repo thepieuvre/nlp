88a9380c98fc725e383eef507c43524d96f3f907
Alexandre di Costanzo <alex@thepieuvre.com>
2013-05-28 00:12:53 +0200

    Pipelining the nlp-low queue for fatser performances
    Many bug fixes: no more infinite loop
    [close #3]

--- src/main/groovy/thepieuvre/nlp/NLProcessor.groovy
+++ src/main/groovy/thepieuvre/nlp/NLProcessor.groovy
@@ -8,28 +8,54 @@ import groovy.json.JsonBuilder
 
 class NLProcessor {
 
-	private LastUpdateList updated = new LastUpdateList(500)
+	private Jedis redis = new Jedis("localhost")
+
+	private LastUpdateList updated = new LastUpdateList(1000)
+
+	private Set toProcess = new TreeSet<Long>()
 
 	private def updatingSimilars(Jedis redis, def similars) {
 		similars.each { id, score ->
-			if (! updated.hasElem(id)) {
+			if (! updated.hasElem(id) && ! toProcess.contains(id)) {
 				redis.rpush("queue:nlp-low", "$id")
 				updated.add(id)
+				println "..... $id pushed for updating"
+ 			} else {
+ 				println "..... $id not repushed"
  			}
 		}
 	}
 
 	def redisMode(String queue) {
 		println "Starting listenning to the $queue"
-		Jedis redis = new Jedis("localhost")
 		redis.sadd('queues', queue)
 		while (true) {
-			def task 
 			try {
-				task = redis.blpop(31415, queue)
-				if (task) {
-					println "${new Date()}.>>>>> ${task[1]}"
-					AnalyzedArticle article = new AnalyzedArticle(task[1] as long)
+				int count = 0
+				def task = redis.blpop(31415, queue)
+				if (queue == 'queue:nlp-low') {
+					while(toProcess.size() < 1000 || task) {
+						count++
+						if (task) {
+							toProcess << (((task.size() == 1)?task[0]:task[1]) as long)
+							println "To Process ${toProcess.size()} on  ${count}"
+						} else {
+							println "no more task"
+							break
+						}
+						task = redis.lpop(queue)
+						println "depoped: $task"
+					}
+				} else {
+					if (task) {
+						toProcess << (task[1] as long)
+					}
+				}
+				println "To process ready to be processed"
+				
+				toProcess.each {
+					println "${new Date()}.>>>>> $it"
+					AnalyzedArticle article = new AnalyzedArticle(it)
 					def builder = new JsonBuilder()
 					builder.nlp {
 						id article.id
@@ -39,11 +65,11 @@ class NLProcessor {
 						similars article.similars
 					}
 					redis.rpush("queue:article", builder.toString())
-					updatingSimilars(redis, article.similars)
+					updatingSimilars(redis, article.similars )
 					println "${new Date()} - Analyzed and pushed to the queue:article"
-				} else {
-					continue
 				}
+				toProcess.clear()
+				count = 0
 			} catch (Exception e) {
 				e.printStackTrace()
 			}
